9747bdc962fe04fb0966b92dfdd2860e027c635f
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-07 06:16:38 +0000

    added 1 file

---
+++ 5.typeConversion.js
@@ -0,0 +1,27 @@
+// Type Conversion
+/*
+Changing datatype from one to another
+
+There are two types
+    1. Implicit
+    2. Explicit
+*/
+/*
+    // Implicit Type Conversion
+        - Any datatype with string using '+' operator converts into string
+            let a = 2 + '1'
+            let b = false + ':'
+            let c = null + 'hi'
+            let d = undefined + '*'
+        
+        - Any datatype with number using '-,*,/,&' operators tries to convert into number and if could not do it then give NaN.
+            let a = '5' - 3
+            let b = true - '5'
+            let c = 'hello' - 'hi'
+*/
+/*
+    // Explicit Type Conversion
+        let a = Number("5")
+        let b = String(true);
+        let c = Boolean(10)
+*/
